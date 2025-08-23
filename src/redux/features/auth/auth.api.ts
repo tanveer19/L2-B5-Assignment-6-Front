2a84cfeef96c66e810d96f17c6c06798baa67332
tanveer19 <thjbd19@gmail.com>
2025-08-23 20:14:42 +0600

    rtk query

---
+++ src/redux/features/auth/auth.api.ts
@@ -0,0 +1,42 @@
+import { baseApi } from "@/redux/baseApi";
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        data: userInfo,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      invalidatesTags: ["USER"],
+    }),
+    register: builder.mutation({
+      query: (userInfo) => ({
+        url: "/user/register",
+        method: "POST",
+        data: userInfo,
+      }),
+    }),
+
+    userInfo: builder.query({
+      query: () => ({
+        url: "/user/me",
+        method: "GET",
+      }),
+      providesTags: ["USER"],
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useLogoutMutation,
+  useUserInfoQuery,
+} = authApi;
---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+import type { ComponentType } from "react";
+
+export type { ILogin } from "./auth.type";
+
+export interface IResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+}
+
+export interface ISidebarItem {
+  title: string;
+  items: {
+    title: string;
+    url: string;
+    component: ComponentType;
+  }[];
+}
+export type TRole = "AGENT" | "ADMIN" | "USER";
